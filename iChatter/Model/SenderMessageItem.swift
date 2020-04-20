c263c625eb1a83ad1cfabbb0e57c1668fc162bb6
vietbq <vietbq@nal.vn>
2020-04-20 23:36:21 +0700

    Message View

---
+++ iChatter/Model/SenderMessageItem.swift
@@ -0,0 +1,16 @@
+//
+//  SenderMessageItem.swift
+//  iChatter
+//
+//  Created by Bui Quoc Viet on 4/20/20.
+//  Copyright © 2020 NAL Viet Nam. All rights reserved.
+//
+
+import Foundation
+
+struct SenderMessageItem {
+    var hiddenNickname = false
+    var hiddenAvatar = false
+    var imgMessageBackground = "ic_left_bottom_message"
+    var urlAvatar = ""
+}
